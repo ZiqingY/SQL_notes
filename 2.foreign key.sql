852ff57cf8b1c706fafec81936b718dcea752ef6
Ziqing Yan <80880595+ZiqingY@users.noreply.github.com>
2022-08-06 23:46:00 +0100

    Update 2.foreign key.sql

--- 2.foreign key.sql
+++ 2.foreign key.sql
@@ -1,31 +1,30 @@
 use mydatabase;
 
-# 后面被引用，是主表；前面是子表
-# 例1：scores表引用了students(StuID), subject(SubjectID)，那么主表是students和subject，子表是scores
-# 例2：students表引用了Grade(GradeId)，那么Grade是主表，studnets是子表
-# 先创建主表，后创建子表；先插入主表数据，后插入子表数据；先删除子表数据，再删除主数据
+/* Parent table is referred. Child table refers
+   Example 1：scores table cites students(StuID), subject(SubjectID), then parent tables are students & subject, child table is scores
+   Example 2：students refers from Grade(GradeId)，thrn Grade is parent table, studnets is child table
+   First create parentT, then childT; First insert for parentT, then childT; First delete childT data, then parentT data. */
 
 DROP TABLE IF EXISTS scores;
 CREATE TABLE `scores`(
 	scoreid 	int(5) primary key auto_increment		comment'primary key',
-    StuID 		int(4) not null							comment'ID of the student',
-	SubjectId 	int(4) not null							comment'ID of the subject',
-    ExamDate 	datetime not null						comment'date of the exam',
-    score 		float(4) not null						comment'exam results',
-	foreign key(StuID) references students(StuID)		# 添加外键的方式一
+    	StuID 		int(4) not null					comment'ID of the student',
+	SubjectId 	int(4) not null					comment'ID of the subject',
+   	ExamDate 	datetime not null				comment'date of the exam',
+    	score 		float(4) not null				comment'exam results',
+	foreign key(StuID) references students(StuID)			/* Method_1 for adding forigen key*/
 );
 describe `scores`;
 
-# 添加外键的方式二。最终scores表格有两个外键（MUL）
+/* Method_2 for forigen key */
 alter table `scores` add constraint `fk_subjectid` foreign key (`SubjectId`) references `Subject`(`SubjectId`);
 
+/* Above codes add 2 forigen keys to scores table: students(StuID), subject(SubjectID)
+   Method_1 adds fkey when creating table; Method_2 adds fkey as constraint to table.
+   After all these you can't add data to fkey column in child table. */
 
-# 以上的两个方式就完成了 scores表的外键：students(StuID), subject(SubjectID)
-# 方式一是自带约束，方式二是自己定义约束constraint
-# 此后不能从子表的foreign key column直接插入数据了
 
-
-# help用法
-help contents; 			#相当于help主菜单
+/* help usage */
+help contents; 
 help `auto_increment`;
 
