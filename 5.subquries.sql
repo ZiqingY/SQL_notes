d9384a663d42593be6decb59d50d51967e5a9806
Ziqing Yan <80880595+ZiqingY@users.noreply.github.com>
2022-08-10 19:51:43 +0100

    Update 5.subquries.sql

--- 5.subquries.sql
+++ 5.subquries.sql
@@ -14,20 +14,20 @@ select * from students order by GradeID desc limit 3;  /* 3 highest year student
 
 
 /* Example 1; inquire info of students who took systematic theology exam */
-# 1. 在subject表中查询美国宪法这门课的SubjectId
+/* Step 1: select sujectID by suject names in suject table */
 select SubjectId from `subject` where SubjectName='systematic theology';
-# 2. 在scores表中查询出美国宪法SubjectId对应的StuId
-select StuId from Scores where SubjectId=(select SubjectId from `subject` where SubjectName='美国宪法');
-# 3. 根据StudId查询出对应的所有学生信息
-select * from Students where StuId in (select StuId from Scores where SubjectId=(select SubjectId from `subject` where SubjectName='美国宪法'));
+/* Step 2: select student ID by suject ID in scores table */
+select StuId from Scores where SubjectId=(select SubjectId from `subject` where SubjectName='systematic theology');
+/* Step 3: select rows by student ID in student info table*/
+select * from Students where StuId in (select StuId from Scores where SubjectId=(select SubjectId from `subject` where SubjectName='systematic theology'));
 
 
-# 例1.1: 可以用 exists(只要后面括号里的命令有输出结果, 就执行前面的命令) 代替 StuID IN, 即:
+/* Example 1.1: can substitue StuID IN with EXISTS */
 SELECT * FROM students WHERE exists (
 SELECT StuID FROM scores WHERE StuId=Students.StuId and SubjectId=(SELECT SubjectId FROM `subject` WHERE SubjectName='美国宪法'));
 
 
-# 例2: 查询参加 "美国宪法"课程 最近一次考试的 学生名单
+/* Example 2 inquire info of students who took systematic theology exam most recently */
 # 1. 从subject表中查询美国宪法这一门课的Id
 SELECT SubjectId FROM `subject` WHERE SubjectName='美国宪法';
 # 2. 根据课程ID从Score中查询对应学生ID
